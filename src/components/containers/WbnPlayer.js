224e86808db2c1cfcf94d80058e1f0526379f6c5
Tatiana Grigorieva <tsg1204@gmail.com>
2020-04-22 08:01:06 -0400

    save to localStorage

--- src/components/containers/WbnPlayer.js
+++ src/components/containers/WbnPlayer.js
@@ -37,6 +37,10 @@ const WbnPlayer = ({ match, history, location }) => {
     })
 
     useEffect(() => {
+        localStorage.setItem(`${state.playlistId}`, JSON.stringify({ ...state}));
+    }, [state])
+
+    useEffect(() => {
         const videoId = match.params.activeVideo;
         if (videoId !== undefined) {
             const newActiveVideo = state.videos.findIndex(
@@ -57,14 +61,41 @@ const WbnPlayer = ({ match, history, location }) => {
     }, [history, location.autoplay, match.params.activeVideo, state.activeVideo.id, state.videos])
 
     const nightModeCallback = () => {
-
+        setState(prevState => ({
+            ...prevState,
+            nightMode: !prevState.nightMode,
+        }))
     }
 
     const endCallback = () => {
+        const videoId = match.params.activeVideo;
+        const currentVideoIndex = state.videos.findIndex(
+            video => video.id === videoId
+        );
+        const nextVideo = currentVideoIndex === state.videos.length - 1 ? 0 : currentVideoIndex + 1;
 
+        history.push({
+            pathname: `/${state.videos[nextVideo].id}`,
+            autoplay: false,
+        })
     }
 
-    const progressCallback = () => {
+    const progressCallback = (e) => {
+        if (e.playedSeconds > 10 && e.playedSeconds < 11) {
+            const videos = [...state.videos];
+            const playedVideo = videos.find(
+                video => video.id === state.activeVideo.id
+            )
+            playedVideo.played = true;
+
+            setState(prevState => ({ ...prevState, videos }))
+            // setState({
+            //     ...state,
+            //     videos: state.videos.map( element => {
+            //         return element.id === state.activeVideo.id ? {...element, played: true } : element;
+            //     })
+            // })
+        }
 
     }
 
